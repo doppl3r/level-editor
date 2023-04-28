115485e243fb3cb0b97a1395c584ce8917e3f75d
Jacob DeBenedetto <deben3@gmail.com>
2023-04-28 07:17:02 -0700

    Add initial properties layout styling

--- src/vue/Properties.vue
+++ src/vue/Properties.vue
@@ -8,7 +8,23 @@
 
 <template>
     <div class="properties">
-        <div class="nav-tabs"></div>
-        <div class="tab-content"></div>
+        <div class="tabs">
+            <ul>
+                <li>
+                    <button class="selected"><span class="icon icon-properties"></span></button>
+                </li>
+                <li>
+                    <button><span class="icon icon-physics"></span></button>
+                </li>
+                <li>
+                    <button><span class="icon icon-texture"></span></button>
+                </li>
+            </ul>
+        </div>
+        <div class="tabs-content">
+            <div class="group">
+                <button><span class="icon icon-opened"></span>Object Properties</button>
+            </div>
+        </div>
     </div>
 </template>
